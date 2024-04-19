584fdc5044b96d1369836bd2499ed3cd0ddc648e
Mauricio Krziminski <mauricio.krziminski@edu.pucrs.br>
2024-04-19 18:10:29 -0300

    feat: Add runtime variable to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ const poppins = Poppins({
   subsets: ['latin'],
 })
 
+export const runtime = 'edge'
+
 export const metadata: Metadata = {
   title: 'Mauricio Krziminski',
   description: 'My Personal Website',
